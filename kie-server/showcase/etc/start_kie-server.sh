4d4fcde2eb2bc30f8fcce09be5d0fe02c59a836f
Leandro Lonardoni <leandro.lonardoni@benner.com.br>
2019-05-21 17:01:28 -0300

    configuração standalone

--- kie-server/showcase/etc/start_kie-server.sh
+++ kie-server/showcase/etc/start_kie-server.sh
@@ -42,13 +42,6 @@ echo "Update database connection setup"
 chmod +x ./update_db_config.sh
 ./update_db_config.sh
 
-sed -i '/<property name="org.kie.server.id" value="$KIE_SERVER_ID"\/>/d' $JBOSS_HOME/standalone/configuration/standalone.xml
-sed -i '/<property name="org.kie.server.location" value="$KIE_SERVER_LOCATION"\/>/d' $JBOSS_HOME/standalone/configuration/standalone.xml
-sed -i '/<property name="org.kie.server.user" value="$KIE_SERVER_USER"\/>/d' $JBOSS_HOME/standalone/configuration/standalone.xml
-sed -i '/<property name="org.kie.server.pwd" value="$KIE_SERVER_PWD"\/>/d' $JBOSS_HOME/standalone/configuration/standalone.xml
-sed -i '/<property name="org.kie.server.controller" value="$KIE_SERVER_CONTROLLER"\/>/d' $JBOSS_HOME/standalone/configuration/standalone.xml
-sed -i '/<property name="org.kie.server.controller.user" value="$KIE_SERVER_CONTROLLER_USER"\/>/d' $JBOSS_HOME/standalone/configuration/standalone.xml
-sed -i '/<property name="org.kie.server.controller.pwd" value="$KIE_SERVER_CONTROLLER_PWD"\/>/d' $JBOSS_HOME/standalone/configuration/standalone.xml
 
 # Start Wildfly with the given arguments.
 echo "Running KIE Execution Server on JBoss Wildfly..."
